2a2b47f279a7063737994388cbbd10d7b8ba9743
CAMHub PublishBot <travis@example.org>
2017-10-17 21:23:00 +0000

    Automated CAMHub::publish on 2017-10-17 21:23:00 UTC

--- chef/cookbooks/linux/metadata.rb
+++ chef/cookbooks/linux/metadata.rb
@@ -3,7 +3,7 @@ maintainer       'IBM Corp'
 maintainer_email ''
 license 'Copyright IBM Corp. 2012, 2017'
 depends 'ibm_cloud_utils'
-version '0.1.42'
+version '1.0.0'
 supports 'redhat'
 description <<-EOH
 
